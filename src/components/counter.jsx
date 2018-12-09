408441074c275769a9e40583185ffa925d598e29
Anna Shvets <annashvets11@gmail.com>
2018-12-09 21:13:38 +0100

    feat: Initial commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+
+    componentDidUpdate(prevProps, prevState){
+        console.log("prevState - ", prevProps);
+        console.log("prevState -", prevState);
+        if (prevProps.counter.value !== this.props.counter.value) {
+            console.log("Make AJAX request to fetch data!");
+        }
+    }
+     
+    styles = {
+        fontSize: 25,
+        fontWeight: "bold"
+    };
+    
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.props.counter.value === 0) ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount(){
+        const { value } = this.props.counter;
+        return value === 0 ? 'Zero' : value;
+    }
+
+     render() {
+        return (
+            <div>
+                 { this.props.children }
+
+                <span 
+                    style={this.styles} 
+                    className={this.getBadgeClasses()}>{this.formatCount()}
+                </span>
+               
+                <button 
+                    onClick={() => this.props.onIncrement(this.props.counter)} 
+                    style={{ fontSize: 25 }} 
+                    className="btn btn-secondary btn-sm">
+
+                    Increment
+                </button>
+
+                <button
+                    onClick={() => this.props.onDelete(this.props.counter.id)}
+                    style={{ fontSize: 25 }}
+                    className="btn btn-danger btn-sm m-2">
+
+                    Delete
+                </button>
+
+            </div>
+        );
+    }
+
+}
+ 
+export default Counter;
---
+++ src/components/counters.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import Counter from './counter';
+
+class Counters extends Component {
+  
+    render() { 
+ 
+        return (
+        <div>
+            <button
+                onClick={this.props.onReset} 
+                style={ {fontSize: 25} }
+                className="btn btn-primary sm m2">
+                Reset
+            </button>
+
+           { this.props.counters.map(counter => (   
+                <Counter 
+                    key={ counter.id } 
+                    onDelete={ this.props.onDelete}
+                    onIncrement={this.props.onIncrement}
+                    counter={counter}>
+
+                    <h4>Counter {counter.id}</h4>
+
+                </Counter>
+            )) }
+        </div>
+        );
+    }
+}
+ 
+export default Counters;
